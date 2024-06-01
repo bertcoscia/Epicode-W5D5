36589fa88f90183b7ac3a9e80ed55c2697095982
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-06-01 16:10:28 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,17 @@ window.onscroll = () => {
     button.style.backgroundColor = "#191919";
   }
 };
+
+const mLetter = document.querySelectorAll("g");
+const mLetterFiltered = Array.from(mLetter).filter((element) => element.getAttribute("opacity") !== undefined);
+
+const mAnimation = setInterval(() => {
+  const randomNum = Math.floor(Math.random() * mLetterFiltered.length);
+  const currentElement = mLetterFiltered[randomNum];
+  const currentOpacity = currentElement.getAttribute("opacity");
+  if (currentOpacity === "0") {
+    currentElement.setAttribute("opacity", 1);
+  } else if (currentOpacity === "1") {
+    currentElement.setAttribute("opacity", 0);
+  }
+}, 40);
